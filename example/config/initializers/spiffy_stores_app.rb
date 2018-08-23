33e3d7dc5904c06754850191885c4e18bdd17b26
Brian Kenn <brian@spiffy.com.au>
2018-08-23 14:30:06 +1000

    Fix typo

--- example/config/initializers/spiffy_stores_app.rb
+++ example/config/initializers/spiffy_stores_app.rb
@@ -1,6 +1,6 @@
 SpiffyStoresApp.configure do |config|
   config.application_name = 'Example App'
-  config.api_key = ENV['SPIFFY_STOREES_CLIENT_API_KEY']
+  config.api_key = ENV['SPIFFY_STORES_CLIENT_API_KEY']
   config.secret = ENV['SPIFFY_STORES_CLIENT_API_SECRET']
   config.scope = 'read_customers, read_orders, write_products'
   config.embedded_app = true
